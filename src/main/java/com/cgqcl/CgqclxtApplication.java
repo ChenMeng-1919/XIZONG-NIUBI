c75b82dbc267f74c053cefa236f0f6f1e16c5dbc
cm <chenmeng_1919@163.com>
2021-05-07 17:38:21 +0800

    项目初始化

---
+++ src/main/java/com/cgqcl/CgqclxtApplication.java
@@ -0,0 +1,13 @@
+package com.cgqcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgqclxtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgqclxtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cgqcl/CgqclxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgqcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgqclxtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
